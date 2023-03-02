4bd2c29210e990dde131c62ab8faf5fb7c91ae38
nicolo fossati <nicofossa01@gmail.com>
2023-03-02 14:54:28 +0100

    input pari dispari

--- PARIDISPARI/js/main.js
+++ PARIDISPARI/js/main.js
@@ -7,15 +7,24 @@ Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 Dichiariamo chi ha vinto.
 */ 
 
-let cond = false;
-
-while(!cond){
-    let input =  parseInt(prompt("Scegli un numero da 1 a 5 compresi"));
-    if((input>=1) && (input<=5)){
-        cond = true;
-    } else {
-        cond = false;
+//scegliere pari o dispari
+let cond1 = false;
+let pd = "";
+while(!cond1){
+    pd = prompt("Scegli e digita \"pari\" o \"dispari\"");
+    if(pd=="pari" || pd=="dispari"){
+        cond1 = true;
     }
 }
 
+//scegliere numero compreso tra 1 e 5
+let cond2 = false;
+let input = "";
+while(!cond2){
+    input =  parseInt(prompt("Scegli un numero da 1 a 5 compresi"));
+    if((input>=1) && (input<=5)){
+        cond2 = true;
+    }
+}
 
+//Genera un numero compreso tra 1 e 5
